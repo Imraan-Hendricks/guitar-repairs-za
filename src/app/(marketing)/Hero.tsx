3175dbe8788826075e884c5bffda107f4f268572
Imraan-Hendricks <imraan.hndx@gmail.com>
2023-07-31 21:18:34 +0200

    chore: perform post-create cleanup

---
+++ src/app/(marketing)/Hero.tsx
@@ -0,0 +1,8 @@
+export function Hero() {
+  return (
+    <section>
+      <h1 className='text-8xl'>Guitar Repair ZA</h1>
+      <p>{`Unlock Your Guitar's True Sound: Precision Repairs that Won't Break the Bank!`}</p>
+    </section>
+  );
+}
