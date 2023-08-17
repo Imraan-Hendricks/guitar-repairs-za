dcdb40401b9692490bdbaddd42715468fce05564
Imraan-Hendricks <imraan.hndx@gmail.com>
2023-08-17 04:15:08 +0200

    chore: create media query utilities

---
+++ src/utils/media-query-utils.ts
@@ -0,0 +1,32 @@
+type Breakpoint = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl';
+type MediaQueryCallback = (matches: boolean) => void;
+
+export function handleMediaQuery(
+  breakpoint: Breakpoint,
+  callback: MediaQueryCallback,
+) {
+  const width =
+    breakpoint === 'xs'
+      ? 0
+      : breakpoint === 'sm'
+      ? 640
+      : breakpoint === 'md'
+      ? 768
+      : breakpoint === 'lg'
+      ? 1024
+      : breakpoint === 'xl'
+      ? 1200
+      : breakpoint === '2xl'
+      ? 1400
+      : 1921;
+
+  const mediaQuery = window.matchMedia(`(min-width: ${width}px)`);
+
+  function handleMediaChange(event: MediaQueryListEvent) {
+    callback(event.matches);
+  }
+
+  mediaQuery.addEventListener('change', handleMediaChange);
+
+  return mediaQuery;
+}
